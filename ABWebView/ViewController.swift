74d56931631eab8575c8c5e0bcd82fce140d77ce
Asif Bilal <asifbilal786@gmail.com>
2017-08-10 15:08:52 +0500

    Initial Commit

---
+++ ABWebView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ABWebView
+//
+//  Created by Asif Bilal on 10/08/2017.
+//  Copyright © 2017 Asif Bilal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
